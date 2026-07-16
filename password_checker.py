31b8b501a90f1c489f9f0572cefa257725cf0b8b
Jovin Wilson <jovinwilson123@gmail.com>
2026-07-16 12:53:22 +0530

    Security level added

--- password_checker.py
+++ password_checker.py
@@ -1,6 +1,37 @@
 import string
 
 
+def check_strength(password):
+
+    score = 0
+
+    if len(password) >= 8:
+        score += 1
+
+    if any(c.islower() for c in password):
+        score += 1
+
+    if any(c.isupper() for c in password):
+        score += 1
+
+    if any(c.isdigit() for c in password):
+        score += 1
+
+    if any(c in string.punctuation for c in password):
+        score += 1
+
+    if score == 5:
+        return "Very Strong"
+
+    elif score == 4:
+        return "Strong"
+
+    elif score == 3:
+        return "Medium"
+
+    else:
+        return "Weak"
+    
 def get_password():
     while True:
         password = input("Enter your password: ")
@@ -14,4 +45,7 @@ def get_password():
 
 password = get_password()
 
-print("Password Accepted!")
+strength = check_strength(password)
+
+print("Password Strength:", strength)
+
